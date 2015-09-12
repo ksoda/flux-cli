e4f04d67eb1c23d606427ccd23169b8ffaf240a1
Ken Sonoda <ken.soda0@gmail.com>
2015-09-12 21:37:43 +0900

    Prevent duplicating processes

--- flux-cli.rb
+++ flux-cli.rb
@@ -4,4 +4,10 @@ require_relative 'flux/geocoder'
 
 Flux::Geocoder.update_location!
 res = Flux::Location.from_yaml
+
+if pid = `pgrep  xflux`.to_i.nonzero?
+	puts "already running in #{pid}"
+	exit 0
+end
+
 exec "#{Flux.root_join 'xflux'} -l #{res.latitude} -k 2000"
--- initialize.rb
+++ initialize.rb
@@ -5,11 +5,6 @@ module Flux
 	end
 end
 
-if pid = `ps -e |grep 'xflux$' |tr -s ' ' |cut -f1 -d' ' `.to_i.nonzero?
-	puts "already running in #{pid}"
-	exit 1
-end
-
 xflux_url = 'https://justgetflux.com/linux/xflux64.tgz'
 unless File.exist? Flux.root_join 'xflux'
   `curl #{xflux_url} | tar -xz -C #{Flux.root_join ''}`
